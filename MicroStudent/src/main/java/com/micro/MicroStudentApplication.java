390700d69bc5d96c36c9419cf0f21ae2e123639f
yhonVillavicencio <54948708+yhonVillavicencio@users.noreply.github.com>
2019-09-24 11:18:23 -0500

    d

--- MicroStudent/src/main/java/com/micro/MicroStudentApplication.java
+++ MicroStudent/src/main/java/com/micro/MicroStudentApplication.java
@@ -3,12 +3,13 @@ package com.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @EnableSwagger2WebFlux
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class MicroStudentApplication {
 
   public static void main(String[] args) {
