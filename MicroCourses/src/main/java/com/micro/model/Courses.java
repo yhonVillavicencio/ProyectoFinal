03667d331207c31fd41ee261a794dedfff37239c
yhonVillavicencio <54948708+yhonVillavicencio@users.noreply.github.com>
2019-09-23 22:07:43 -0500

    d

--- MicroCourses/src/main/java/com/micro/model/Courses.java
+++ MicroCourses/src/main/java/com/micro/model/Courses.java
@@ -1,5 +1,33 @@
 package com.micro.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "courses")
 public class Courses {
+	
+	@Id
+	private String id;
+	
+	private String nombreCurso;
+	
+	private String numeroIdentificacionStudent;
+	
+	private String numeroIdentificacionTeacher;
+	
+	private String estadoCurso;
+	
+	private int numeroStudent;
+	
+	
+	
+	
 
 }
--- MicroTeachers/src/main/java/com/micro/model/Teachers.java
+++ MicroTeachers/src/main/java/com/micro/model/Teachers.java
@@ -1,5 +1,31 @@
 package com.micro.model;
 
-public interface Teachers {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "teachers")
+public class Teachers {
+	
+	@Id
+	private String id;
+	
+	private String tipoIdentificacion;
+	
+	private String numeroIdentificacion;
+	
+	private String nombre;
+	
+    private String genero;
+	
+	
+	
+	
 
 }
