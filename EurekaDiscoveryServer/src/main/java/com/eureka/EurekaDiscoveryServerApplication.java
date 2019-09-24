72b71aafa7c86fa0e4f304ba4d2bcbe13d57a7db
yhonVillavicencio <54948708+yhonVillavicencio@users.noreply.github.com>
2019-09-24 10:52:10 -0500

    d

--- EurekaDiscoveryServer/src/main/java/com/eureka/EurekaDiscoveryServerApplication.java
+++ EurekaDiscoveryServer/src/main/java/com/eureka/EurekaDiscoveryServerApplication.java
@@ -3,8 +3,9 @@ package com.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDiscoveryServerApplication {
 
 	public static void main(String[] args) {
