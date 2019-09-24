41c0c00927be7223bada062a3b14ce619ea3bdce
yhonVillavicencio <54948708+yhonVillavicencio@users.noreply.github.com>
2019-09-23 20:44:49 -0500

    d

--- MicroStudent/src/main/java/com/micro/MicroStudentApplication.java
+++ MicroStudent/src/main/java/com/micro/MicroStudentApplication.java
@@ -2,11 +2,13 @@ package com.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @EnableSwagger2WebFlux
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroStudentApplication {
 
   public static void main(String[] args) {
