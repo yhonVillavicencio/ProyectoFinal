7561a8fb5f1e78e7463473b00fbc691d230bd0c5
yhonVillavicencio <54948708+yhonVillavicencio@users.noreply.github.com>
2019-09-24 15:26:27 -0500

    D

--- MicroservicioFamilyMembers/src/main/java/com/microservicio/MicroservicioFamilyMembersApplication.java
+++ MicroservicioFamilyMembers/src/main/java/com/microservicio/MicroservicioFamilyMembersApplication.java
@@ -2,11 +2,14 @@ package com.microservicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @EnableSwagger2WebFlux
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicioFamilyMembersApplication {
 
 	public static void main(String[] args) {
